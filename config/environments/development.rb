fbcaa2b72c896fa72f5503f3791f396df8cd70ec
Rodrigo Gomes <rgomes.rodrigo@gmail.com>
2012-07-21 21:59:28 -0300

    configurando o action mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,16 @@ Devise2::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'innvent',
+    :user_name            => 'rgomes.rodrigo',
+    :password             => 'xxx',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
+
 end
